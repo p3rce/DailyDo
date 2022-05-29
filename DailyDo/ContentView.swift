2e284f7522d91edbdd2cf077079cf725a10cf9dc
Pierce Goulimis <piercemg2013@gmail.com>
2022-05-29 15:59:19 -0400

    Initial Commit

---
+++ DailyDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyDo
+//
+//  Created by Pierce Goulimis on 2022-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DailyDo/DailyDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDoApp.swift
+//  DailyDo
+//
+//  Created by Pierce Goulimis on 2022-05-29.
+//
+
+import SwiftUI
+
+@main
+struct DailyDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
